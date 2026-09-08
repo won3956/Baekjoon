0105a4010db578eb8f4800b02492ef844214c4ee
JiwonMoon <132432108+won3956@users.noreply.github.com>
2026-09-08 11:38:57 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 4,057 ms, Memory: 27,496 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -2,52 +2,55 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+
 public class Solution {
-	static int[] gyu, iny, isUsed;
-	static int idx;
-	static int vicCount, defCount;
-	static int win, lose; //규영이가 이긴횟수와 진횟수
+	static int[] gyu, iny;
+	static boolean[] isUsed;
+	static int winGyu, winIn;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		for(int tc=1; tc<=T; tc++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			vicCount = 0; defCount = 0;
+			winGyu = 0; winIn = 0;
 			gyu = new int[9];
 			iny = new int[9];
-			isUsed = new int[19];
+			isUsed = new boolean[19];
+			
 			for(int i = 0; i < 9; i++) {
 				int input = Integer.parseInt(st.nextToken());
 				gyu[i] = input;
-				isUsed[input] = i+1;
+				isUsed[input] = true;
 			}
+			
 			perm(0);
-			System.out.println("#"+tc+" "+vicCount+" "+defCount);
+			System.out.println("#"+tc+" "+winGyu+" "+winIn);
 		}
 	}
-	public static void perm(int count) {
-		if(count == 9) {
+	public static void perm(int cnt) {
+		if(cnt == 9) {
 			cal();
 			return;
 		}
 		
 		for (int i = 1; i <= 18; i++) {
-			if(isUsed[i]>0) continue;
-			iny[count] = i;
-			isUsed[i] = count + 1;
-			perm(count + 1);
-			iny[count] = 0;
-			isUsed[i] = 0;
+			if(isUsed[i]) continue;
+			
+			iny[cnt] = i;
+			isUsed[i] = true;
+			
+			perm(cnt + 1);
+			isUsed[i] = false;
 		}
 	}
 	public static void cal() {
 		int sumIn = 0; int sumGyu = 0;
 		for (int i = 0; i < 9; i++) {
-			int sum = iny[i] - gyu[i];
-			if(sum > 0) sumIn += (iny[i] + gyu[i]);
-			if(sum < 0) sumGyu += (iny[i] + gyu[i]);
+			if(iny[i] > gyu[i]) sumIn += (iny[i] + gyu[i]);
+			if(iny[i] < gyu[i]) sumGyu += (iny[i] + gyu[i]);
 		}
-		if(sumIn > sumGyu) ++defCount;
-		else if(sumIn < sumGyu) ++vicCount;
+		if(sumIn > sumGyu) ++winIn;
+		else if(sumIn < sumGyu) ++winGyu;
 	}
 }
